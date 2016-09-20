3546a621ea1c11549dafb6432aa0a3c29c0c8b81
Raditzz <raditya.danang@gmail.com>
2016-09-20 16:46:06 +0700

    2016-09-20_16-46-06

--- js/myJs.js
+++ js/myJs.js
@@ -1,3 +1,8 @@
 $( document ).ready(function() {
   $('.container').addClass("animated fadeIn");
+
+  $('.socmed').hover(
+       function(){ $(this).addClass('animated pulse') },
+       function(){ $(this).removeClass('animated pulse') }
+  )
 });
